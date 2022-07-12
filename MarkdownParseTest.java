e4fbaa57ec870725ef3c44b1b370a0d3440df618
JinhanLan <108479051+JinhanLan@users.noreply.github.com>
2022-07-12 08:16:35 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,5 +6,26 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    
+      @Test
+    public void testtestfile() throws IOException {
+        String contents = Files.readString(Path.of("/Users/irislan/Documents/GitHub/markdown-parser-1/test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-thing.html");
+        assertEquals(expect,MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testtestfile2() throws IOException {
+        String contents = Files.readString(Path.of("/Users/irislan/Documents/GitHub/markdown-parser-1/test-file2.md"));
+        List<String> expect = List.of("https://google.com", "some-thing.html");
+        assertEquals(expect,MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testtestfile3() throws IOException {
+        String contents = Files.readString(Path.of("/Users/irislan/Documents/GitHub/markdown-parser-1/test-file3.md"));
+        List<String> expect = List.of("https://google.com", "some-thing.html");
+        assertEquals(expect,MarkdownParse.getLinks(contents));
+    }
 
 }
